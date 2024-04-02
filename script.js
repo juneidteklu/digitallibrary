4033788e74f6294e452a272b0a8e5e4081707074
Juneid Teklu <te.juneid@gmail.com>
2024-04-02 18:23:20 -0500

    Create event listener that creates a resource div everytime it is clicked

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let resourceCategory = document.querySelector(".add-category")
+let resourceCategoryButton = document.querySelector(".add-category")
+let resourceContainer = document.querySelector(".resource-container")
 
 function createCategory(){
+    let container = document.createElement("div");
+    container.classList.add("container")
     let resource = document.createElement("div");
     resource.classList.add("resource");
     let span = document.createElement("span");
+    span.textContent = "Resource";
     let button = document.createElement("button");
+    button.textContent = "Add a resource"
     let ul = document.createElement("ul");
-    button.classList.add("add-resource")
+    button.classList.add("add-resource");
+    resource.append(span,button,ul);
+    container.appendChild(resource);
+    resourceContainer.appendChild(container);
 }
+
+resourceCategoryButton.addEventListener("click", createCategory);
